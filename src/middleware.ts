f55448b5f9f15d5947574e54e2630de418394ef1
Prathamesh Chougale <prathamesh17170@gmail.com>
2024-09-08 14:19:44 +0530

    refactor: Update Navbar component with new UI elements and responsive design

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,3 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
 /*
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 const isProtectedRoute = createRouteMatcher([
@@ -15,8 +14,19 @@ const isProtectedRoute = createRouteMatcher([
   });
 
 */
-export default clerkMiddleware()
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
+const isPublicRoute = createRouteMatcher([
+  '/sign-in(.*)', 
+  '/sign-up(.*)',
+  '/'
+])
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect()
+  }
+})
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
