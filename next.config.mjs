401da6b7108d29c2b80be68dfc0fd0bcf2e93349
Prathamesh Chougale <prathamesh17170@gmail.com>
2024-09-09 15:50:49 +0530

    chore: Update next.config.mjs to include remotePatterns for Microlink Image Preview

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-          "api.microlink.io", // Microlink Image Preview
-        ],
+        remotePatterns:[
+          {
+            protocol: 'https',
+            hostname:  "api.microlink.io"
+          }
+        ]
       },
 };
 
