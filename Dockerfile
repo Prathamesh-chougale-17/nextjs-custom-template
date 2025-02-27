d67e5d8e5f13f9e854cb36bf7de42d15af9a2de0
Prathamesh Chougale <prathamesh17170@gmail.com>
2025-02-27 15:14:40 +0530

    refactor: simplify Dockerfile and update middleware for public route handling

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,3 @@
-# FROM node:alpine3.20
-
-# # Create app directory
-
-# WORKDIR /app
-
-# COPY . .
-
-# RUN npm install
-
-# RUN npm run build
-
-# # EXPOSE 3000
-
-# CMD ["npm", "start"]
-
 FROM oven/bun:alpine AS base
 
 # Stage 1: Install dependencies
--- middleware.ts
+++ middleware.ts
@@ -1,37 +1,24 @@
-/*
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-const isProtectedRoute = createRouteMatcher([
-    '/ai(.*)',
-    '/contact(.*)',
-    '/games(.*)',
-  ]);
-  
-  export default clerkMiddleware((auth, req) => {
-    if (!auth().userId && isProtectedRoute(req)) {
-      // Add custom logic to run before redirecting
-      return auth().redirectToSignIn();
-    }
-  });
-
-*/
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
-  '/sign-in(.*)', 
-  '/sign-up(.*)',
-  '/'
-])
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/",
+  // json files are public
+  "/(.*).json",
+]);
 
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
-    auth().protect()
+    auth().protect();
   }
-})
+});
+
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Skip Next.js internals, all static files, and include JSON as public
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
-}
+};
